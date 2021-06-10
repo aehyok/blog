25da7836650747eb709dc9c277a1e4c2fc0083c6
刘启明 <455043818@qq.com>
2021-06-10 16:30:17 +0800

    commit(6月10日)

--- 2021-06-09-build.sh
+++ 2021-06-09-build.sh
@@ -5,10 +5,10 @@ current_path=$(cd $(dirname $0); pwd)
 echo $current_path
 
 # 打印当前目录文件列表
-echo $a* 
+# echo $a* 
 
+###############1、需要拉取的项目路径##############
 
-## 需要拉取的项目路径
 gitpull_pathArr=(
   "/f/work/git/dvs-2.x/dvs-app-h5-develop"
   "/f/work/git/dvs-2.x/dvs-offiaccount-dev"
@@ -16,8 +16,7 @@ gitpull_pathArr=(
   "/f/work/git/dvs-2.x/dvs-server-ui-dev"
 )
 
-
-## 开始拉取
+## 开始pull拉取项目
 for ((i=0;i<${#gitpull_pathArr[*]};i++))
 do
   # project_path=${current_path}/${project_pathArr[i]}
@@ -28,7 +27,7 @@ do
   echo -e "\033[32m拉取项目：${gitpull_pathArr[i]} 成功\033[0m";
 done
 
-## 需要拉取的项目路径
+#############2、需要编译的项目路径
 npmbuild_pathArr=(
   "/f/work/git/dvs-2.x/dvs-app-h5-develop"
   "/f/work/git/dvs-2.x/dvs-offiaccount-dev"
@@ -39,15 +38,54 @@ npmbuild_pathArr=(
   "/f/work/git/dvs-2.x/dvs-server-ui-dev/dvs-village"
   "/f/work/git/dvs-2.x/dvs-server-ui-dev/dvs-digital"
 )
-## 开始拉取
+## 开始 build 项目
 for ((i=0;i<${#npmbuild_pathArr[*]};i++))
 do
-  # project_path=${current_path}/${project_pathArr[i]}
   project_path=${npmbuild_pathArr[i]}
   cd $project_path
   yarn build
   echo -e "\033[32m编译项目：${npmbuild_pathArr[i]} 成功\033[0m";
 done
+
+
+############3、删除之前打包的文件
+rm_pathArr=(
+  "/f/work/git/dvs-2.x/dvs-release-dev/cms/app/"
+  "/f/work/git/dvs-2.x/dvs-release-dev/cms/qrcode/"
+  "/f/work/git/dvs-2.x/dvs-release-dev/cms/wechat/"
+  "/f/work/git/dvs-2.x/dvs-release-dev/cms/console/"
+)
+
+for ((i=0;i<${#rm_pathArr[*]};i++))
+do
+  project_path=${rm_pathArr[i]}
+  cd $project_path
+  rm -rf *
+  echo -e "\033[32mcp删除项目：${rm_pathArr[i]} 成功\033[0m";
+done
+
+
+###########4、复制某个文件夹到某个文件夹下（目标文件夹好像要存在）
+##复制某个文件到某个目录下面
+# cp build.sh image/
+
+copy_pathArr=(
+  "/f/work/git/dvs-2.x/release/cms/app/"
+  "/f/work/git/dvs-2.x/release/cms/qrcode/"
+  "/f/work/git/dvs-2.x/release/cms/wechat/"
+  "/f/work/git/dvs-2.x/release/cms/console/"
+)
+
+## 开始 copy拷贝目录
+for ((i=0;i<${#copy_pathArr[*]};i++))
+do
+  project_path=${copy_pathArr[i]}
+  cd $project_path
+  cp  -R  $project_path  /f/work/git/dvs-2.x/dvs-release-dev/cms/
+  echo -e "\033[32mcp拷贝项目：${copy_pathArr[i]} 成功\033[0m";
+done
+
+# cp  -R  /f/work/git/dvs-2.x/release/cms/  /f/work/git/dvs-2.x/dvs-release-dev/cms/
 
 
 echo "5秒后将自动退出本脚本："
@@ -56,3 +94,5 @@ do
   echo -e $i;sleep 1
 done
 exit
+
+## 执行脚本  sh 2021-06-09-build.sh
