672d5e054ff19643c66cbe1955a152ab854782bc
aehyok <455043818@qq.com>
2022-04-10 19:30:35 +0800

    Update index.mjs

--- zx/index.mjs
+++ zx/index.mjs
@@ -19,6 +19,9 @@ const windowArray =[
 const macArray =[]
  
 const isMac = () => {
+    // win32 代表window平台
+    // darwin 代表mac平台
+    // 
     return process.platform === 'darwin'? true : false
 }
 
