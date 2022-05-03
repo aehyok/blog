ddd2d0dffa3ffb3874fe471f26591e60b58d584a
aehyok <455043818@qq.com>
2022-05-03 23:55:25 +0800

    feat: 可写日志log，版本，待优化

--- zx/build-app.mjs
+++ zx/build-app.mjs
@@ -1,12 +1,20 @@
 import {$, cd } from "zx";
 import { sleep } from "zx";
 import { baseUrl } from "./utils/index.mjs";
+const fs = require('fs-extra')
+var os = require('os')
 
-const project = 'app-h5'
+fs.appendFile('log', 'Node.js中文网'+ os.EOL, 'utf8', (err) => {
+    if (err) throw err; // 若写入错误，则抛出err
+    console.log('文件已被保存'); // 若写入成功，则打印‘文件已被保存’
+  });
+  
+const project = 'vue-qiankun'
 const gitProject = baseUrl + project
+const main = gitProject + '/' + 'main-vite-app-ts'
 const appList = [
-    gitProject + '/' + 'main-app',
-    gitProject + '/' + 'ffp-app'
+    gitProject + '/' + 'webpack-app-ts',
+    gitProject + '/' + 'map-app-ts',
 ]
 export const build_app = async(version) => {
     cd(gitProject)
@@ -14,15 +22,29 @@ export const build_app = async(version) => {
     // RSA key fingerprint is SHA256:ZrA2ZqYTVyPbw4zytCSAv74ZMaS2LDH74I7sMPtQIG0.
     // Are you sure you want to continue connecting (yes/no/[fingerprint])?
     
-    await $`git pull`
-    if ($`$?` == 0)
+    let pullInfo = await $`git pull`
+    console.log(pullInfo, 'pullInfo')
+    if (pullInfo.exitCode == 0) {
         console.log('success')
-    else
-        console.log('fail')
+    }
+    else {
+        console.log('fail',$`$?`)
+    }
   
-    appList.forEach(element => {
-        single(element)
-    });
+    // appList.forEach(element => {
+    //     single(element)
+    // });
+
+    cd(main)
+    console.log('before')
+    let buildInfo = await $`pnpm build` 
+    console.log('after')
+    console.log(buildInfo, 'buildInfo')
+    // Promise.all(
+    //     appList.map(item => {
+    //         return $`cd ${item}; pnpm build`
+    //     })
+    // )
     
     console.log('开始编译app的', version)
 }
@@ -32,4 +54,5 @@ export const build_app = async(version) => {
 const single = async(element) => {
     cd(element)
     await $`pwd`  
+    await $`pnpm start`
 }
--- zx/utils/index.mjs
+++ zx/utils/index.mjs
@@ -1,7 +1,7 @@
 /**
  * dvs项目基础路径
  */
-export const baseUrl = '/Users/tanghongling/Desktop/aehyok/shengyang/dvs/'
+export const baseUrl = '/Users/tanghongling/Desktop/aehyok/github/'
 
 export const isMac = () => {
     // win32 代表window平台
