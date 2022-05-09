3d89b54e94c0038fa2dc9e1b9aed1e8355aa9e39
刘启明 <455043818@qq.com>
2022-05-09 23:43:07 +0800

    feat： commit（5月9日）

--- zx/build-one.mjs
+++ zx/build-one.mjs
@@ -2,22 +2,30 @@ import { baseUrl } from "./utils/index.mjs";
 import { currentLogger } from "./utils/log.mjs";
 
 const current = await $`pwd`;
-const project = "dvs-offiaccount-dev";
-const gitProject = baseUrl + project;
-const main = gitProject;
+// const project = "dvs-offiaccount-dev";
+let gitProject = '' // baseUrl + project;
+let main = '' // gitProject;
 
 const oneLogger = (info) => {
     currentLogger(current, info)
 }
 
-export const build = async (version) => {
-  console.log("开始编译PC的", version);
+const init = (project) => {
+  console.log(project, 'project');
+  gitProject = baseUrl + project
+  main = gitProject
+}
+
+export const build = async (version, project) => {
+  oneLogger(`开始初始化:${project}`);
+  init(project)
   await gitPull();
   await buildone();
 };
 
 const gitPull = async () => {
   try {
+    oneLogger(`开始git pull:${gitProject}`);
     const gitPull = await $`cd ${gitProject}; git pull;`;
     console.log(gitPull, "pullInfo");
     if (gitPull.exitCode == 0) {
@@ -37,9 +45,9 @@ const buildone = async () => {
     const buildInfo = await $` cd ${main};yarn build;`;
     console.log(buildInfo, "buildInfo");
     if (buildInfo.exitCode === 0) {
-        oneLogger(current, `success build ${gitProject}`);
+        oneLogger(`success build ${gitProject}`);
     } else {
-        oneLogger(current, `build main-app error: ${buildInfo.stderr}`);
+        oneLogger(`build main-app error: ${buildInfo.stderr}`);
     }
   } catch {
     oneLogger("buildAppMain 发生错误");
--- zx/index.mjs
+++ zx/index.mjs
@@ -7,6 +7,38 @@ import { build } from "./build-one.mjs";
 import { currentLogger } from './utils/log.mjs'
 $.verbose = true;
 const current = await $`pwd`;
+// gitpull_pathArray["a"]="/e/work/git/dvs-2.x/dvs-app-h5-develop"
+// gitpull_pathArray["w"]="/e/work/git/dvs-2.x/dvs-offiaccount-dev"
+// gitpull_pathArray["p"]="/e/work/git/dvs-2.x/dvs-park-h5-app"
+// gitpull_pathArray["q"]="/e/work/git/dvs-2.x/qrcode-demo-dev"
+// gitpull_pathArray["c"]="/e/work/git/dvs-2.x/dvs-server-ui-dev"
+let projectList = [
+  {
+    shortName: 'pc',
+    fullName: 'dvs-server-ui-dev',
+    isqiankun: true
+  },
+  {
+    shortName: 'app',
+    fullName: 'dvs-app-h5-develop',
+    isqiankun: true
+  },
+  {
+    shortName: 'wechat',
+    fullName: 'dvs-offiaccount-dev',
+    isqiankun: false
+  },
+  {
+    shortName: 'park',
+    fullName: 'dvs-park-h5-app',
+    isqiankun: false
+  },
+  {
+    shortName: 'qrcode',
+    fullName: 'qrcode-demo-dev',
+    isqiankun: false
+  }
+]
 
 // 获取版本信息
 console.log(argv.v, "v");
@@ -26,6 +58,9 @@ if(projectName === 'pc') {
 if(projectName === 'app') {
   build_app(version)
 }
-if(projectName === 'wechat') {
-  build(version)
+let currentProject = projectList.find(item => !item.isqiankun && item.shortName === projectName)
+console.log(currentProject, 'currentProject');
+if(currentProject && Object.keys(currentProject).length > 0) {
+  // projectList.find(item => item.shortName === projectName)
+  build(version, projectList.find(item => item.shortName === projectName).fullName)
 }
