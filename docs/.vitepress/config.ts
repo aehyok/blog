30abdf6810c42483aae31e37424d68d67fef7026
aehyok <455043818@qq.com>
2022-07-25 11:49:33 +0800

    chore: update Nav顶部菜单

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -11,43 +11,62 @@ export default defineConfig({
     logo: "/logo.svg",
     nav: [
       { text: "首页", link: "/" },
-      { text: "每日笔记", link: "/days/" },
-      { text: "月度笔记", link: "/months/" },
+      { text: "我的笔记", link: "/daily/" },
+      { text: "我的笔记", link: "/javascript/" },
+      { text: "生活感悟", link: "/life/" },
       { text: "我的掘金", link: "https://juejin.cn/user/2242659452477016" },
       { text: "关于我", link: "/me/" },
     ],
     socialLinks: [{ icon: "github", link: "https://github.com/aehyok" }],
     footer: {
       message: "Released under the MIT License.",
-      copyright: "Copyright © 2022-present aehyok",
+      copyright: "Copyright © 2022-07～present aehyok",
     },
 
     sidebar: {
-      "/days/": [
+      "/life/": [
         {
-          text: "每日笔记",
+          text: '生活感悟',
           items: [
-            { text: "2022-07", link: "/days/" },
-            { text: "2022-06", link: "/days/2022-06" },
-            { text: "2022-05", link: "/days/2022-05" },
-            { text: "2022-04", link: "/days/2022-04" },
-            { text: "2022-03", link: "/days/2022-03" },
-            { text: "2022-02", link: "/days/2022-02" },
-            { text: "2022-01", link: "/days/2022-01" },
+            { text: "社会", link: "/life/" },
+            { text: "2022-06年中总结", link: "/life/2022-06" },
+            { text: "2021-12年终总结", link: "/life/2021-12" },
+          ]
+        }
+      ],
+      "/javascript/": [
+        {
+          text: 'JavaScript积累',
+          items: [
+            { text: "社会", link: "/javascript/" },
+            { text: "2022-06年中总结", link: "/life/2022-06" },
+            { text: "2021-12年终总结", link: "/life/2021-12" },
+          ]
+        }
+      ],
+      "/daily/": [
+        {
+          text: "2022年每日笔记",
+          items: [
+            { text: "2022-07", link: "/daily/" },
+            { text: "2022-06", link: "/daily/2022-06" },
+            { text: "2022-05", link: "/daily/2022-05" },
+            { text: "2022-04", link: "/daily/2022-04" },
+            { text: "2022-03", link: "/daily/2022-03" },
+            { text: "2022-02", link: "/daily/2022-02" },
+            { text: "2022-01", link: "/daily/2022-01" },
           ],
         },
-      ],
-      "/months/": [
         {
-          text: "月度笔记",
+          text: "2021年每日笔记",
           items: [
-            { text: "2022-07", link: "/months/" },
-            { text: "2022-06", link: "/months/2022-06" },
-            { text: "2022-05", link: "/months/2022-05" },
-            { text: "2022-04", link: "/months/2022-04" },
-            { text: "2022-03", link: "/months/2022-03" },
-            { text: "2022-02", link: "/months/2022-02" },
-            { text: "2022-01", link: "/months/2022-01" },
+            { text: "2021-07", link: "/daily/" },
+            { text: "2021-06", link: "/daily/2022-06" },
+            { text: "2021-05", link: "/daily/2022-05" },
+            { text: "2021-04", link: "/daily/2022-04" },
+            { text: "2021-03", link: "/daily/2022-03" },
+            { text: "2021-02", link: "/daily/2022-02" },
+            { text: "2021-01", link: "/daily/2022-01" },
           ],
         },
       ],
--- src/components/HomeAbout.vue
+++ src/components/HomeAbout.vue
@@ -1,51 +1,37 @@
-name: Auto Deploy # 当前工作流程的名称
-on: 
-  push: 
-    branches: 
-      - main # 只要push到main分支，就会触发该工作流程
-jobs:
-  build-and-deploy:
-    runs-on: ubuntu-latest
-    steps:
-      # 使用别人封装好的的action，用于clone该仓库的源码到工作流中
-      - name: Checkout
-        uses: actions/checkout@v3
-​
-      # 在工作流中安装node环境（必需，这样才能在后续工作流程中运行 npm install 等指令，否则会报错）
-      - name: Setup node
-        uses: actions/setup-node@v3
-        with:
-          node-version: 16 # 指定node版本
-          
-      # 打包代码生成环境
-      - name: Build
-        run: |
-          cd client # 进入前端侧的工程目录
-          npm install # 安装依赖
-          npm run build # 打包前端代码到生产环境（目标路径为：server/public）
-​
-      # 同步server目录下的后端代码到服务器（目标路径：/home/nginx/myBlogServer）
-      - name: Deploy
-        uses: cross-the-world/scp-pipeline@master
-        with:
-          host: ${{ secrets.MY_HOST }} # 服务器IP（需要在GitHub上自行配置对应的secret）
-          user: ${{ secrets.MY_USER }} # 服务器用户名
-          pass: ${{ secrets.MY_PASS }} # 服务器密码
-          connect_timeout: 10s
-          local: './server/*' # 源路径（工作流）
-          remote: /home/nginx/myBlogServer # 目标路径（服务器）
-      
-      # 在服务器端执行相关指令
-      - name: Executing remote ssh commands
-        uses: appleboy/ssh-action@master
-        with:
-          host: ${{ secrets.MY_HOST }} # 服务器IP（需要在GitHub上自行配置对应的secret）
-          username: ${{ secrets.MY_USER }} # 服务器用户名
-          password: ${{ secrets.MY_PASS }} # 服务器密码
-          script: |
-            cd /home/nginx/myBlogServer # 进入服务器中的端工程所在的目录
-            export NODE_HOME=/root/.nvm/versions/node/v12.19.0  # 可使用`whereis node`查询node所在的目录
-            export PATH=$PATH:$NODE_HOME/bin # 重新定义node的环境变量（必需，这样才能在后续工作流程中运行 npm install 等指令，否则会报错）
-            npm install # 安装项目依赖
-            pm2 delete myBlogServer # 删除旧的进程
-            pm2 start --name myBlogServer npm -- run server # 启动新的进程
+<template>
+  <div className="home-about">
+    <div className="container">
+      <img src="../../docs/public/logo.svg" alt="love" />
+      <h2 className="message">Use <span @click="goVitePress()">VitePress</span> Build</h2>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+  const goVitePress = () => window.open('https://vitepress.vuejs.org/','_blank')
+</script>
+<style lang="scss" scoped>
+.home-about {
+  padding-top: 90px;
+  margin-top: 50px;
+  border-top: 1px solid var(--vp-c-divider-light);
+
+  .container {
+    color: var(--vp-c-text-2);
+    text-align: center;
+    img {
+      margin: 0 auto;
+      margin-bottom: 15px;
+      filter: drop-shadow(#fff 10px 0);
+    }
+
+    span {
+      color: var(--vp-c-brand);
+      cursor: pointer;
+
+      &:hover {
+        color: var(--vp-c-brand-dark);
+      }
+    }
+  }
+}
+</style>
