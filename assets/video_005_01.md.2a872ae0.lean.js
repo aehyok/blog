68519b76772d031964418ef0e208dee5a9e438ec
aehyok <aehyok@users.noreply.github.com>
2023-03-28 14:01:42 +0000

    deploy: 0f1474aa34f82ab3f0991c6e9572719f5bb0dd0a

---
+++ assets/video_005_01.md.2a872ae0.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as a,N as r}from"./chunks/framework.294d0221.js";const f=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"video/005/01.md","lastUpdated":1680011906000}'),o={name:"video/005/01.md"},s=r("",4),c=[s];function n(h,d,l,i,p,u){return t(),a("div",null,c)}const x=e(o,[["render",n]]);export{f as __pageData,x as default};
