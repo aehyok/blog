1d6c3c85ab282f5ec833e1abd1369cdce9d80934
aehyok <aehyok@users.noreply.github.com>
2023-03-29 10:32:59 +0000

    deploy: b9b605f3db56140e6164f86afb4985872ec6e39e

---
+++ assets/daily_2022-12.md.ac2701d4.lean.js
@@ -0,0 +1,1 @@
+import{_ as l,o as e,c as a,N as t}from"./chunks/framework.294d0221.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"daily/2022-12.md","lastUpdated":1680085796000}'),r={name:"daily/2022-12.md"},i=t("",42),n=[i];function o(s,h,c,u,p,d){return e(),a("div",null,n)}const g=l(r,[["render",o]]);export{m as __pageData,g as default};
