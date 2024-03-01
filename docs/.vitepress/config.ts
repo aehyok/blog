49ed9f396615a8f23486adb7ac4d0d4656811cbe
刘启明 <455043818@qq.com>
2024-03-01 18:34:10 +0800

    chore:update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -61,6 +61,7 @@ export default defineConfig({
           text: "2024年每日笔记",
           items: [
             { text: "current", link: "/daily/" },
+            { text: "2024-03", link: "/daily/2024-03" },
             { text: "2024-02", link: "/daily/2024-02" },
             { text: "2024-01", link: "/daily/2024-01" },
           ],
