c1bcef6859f275131a60dd95f5ae4116ea25f182
aehyok <aehyok@users.noreply.github.com>
2024-04-05 12:05:52 +0000

    deploy: 02538ac7ffdba4821edea7ea1c206b07be1698ea

---
+++ assets/life_2023-05-22.md.d9cde68f.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,c as p,b as _}from"./app.f8774bdb.js";const A=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"life/2023-05-22.md","lastUpdated":1712318446000}'),a={name:"life/2023-05-22.md"},o=_("",10),s=[o];function c(n,r,d,i,l,f){return e(),p("div",null,s)}const T=t(a,[["render",c]]);export{A as __pageData,T as default};
