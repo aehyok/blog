b6f6e75e85776a123ef61d29a481eae46e90e536
aehyok <aehyok@users.noreply.github.com>
2024-12-06 12:03:35 +0000

    deploy: 9ce6248d8556fafb3600cfd9f302b7a002f9486b

--- assets/javascript_2024-11-29-java.md.986EcE3c.js
+++ assets/javascript_2024-11-29-java.md.986EcE3c.js
@@ -1,4 +1,4 @@
-import{_ as a,c as s,o as n,a5 as p}from"./chunks/framework.CW9jv5bj.js";const g=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"javascript/2024-11-29-java.md","filePath":"javascript/2024-11-29-java.md"}'),t={name:"javascript/2024-11-29-java.md"},e=p(`<h2 id="登录" tabindex="-1">登录 <a class="header-anchor" href="#登录" aria-label="Permalink to &quot;登录&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>// Swagger快捷登录json</span></span>
+import{_ as a,c as s,o as n,a5 as p}from"./chunks/framework.CW9jv5bj.js";const g=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"javascript/2024-11-29-java.md","filePath":"javascript/2024-11-29-java.md"}'),e={name:"javascript/2024-11-29-java.md"},t=p(`<h2 id="登录" tabindex="-1">登录 <a class="header-anchor" href="#登录" aria-label="Permalink to &quot;登录&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>// Swagger快捷登录json</span></span>
 <span class="line"><span>{</span></span>
 <span class="line"><span>  &quot;userName&quot;: &quot;admin&quot;,</span></span>
 <span class="line"><span>  &quot;password&quot;: &quot;admin123456!&quot;,</span></span>
@@ -13,4 +13,9 @@ import{_ as a,c as s,o as n,a5 as p}from"./chunks/framework.CW9jv5bj.js";const g
 <span class="line"><span>systemctl restart sun-xxm-java</span></span>
 <span class="line"><span></span></span>
 <span class="line"><span>// 访问地址</span></span>
-<span class="line"><span>http://101.200.243.192:624/swagger-ui/index.html</span></span></code></pre></div>`,4),l=[e];function i(o,c,r,u,d,h){return n(),s("div",null,l)}const q=a(t,[["render",i]]);export{g as __pageData,q as default};
+<span class="line"><span>http://101.200.243.192:624/swagger-ui/index.html</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>// 前端 打包</span></span>
+<span class="line"><span>pnpm build</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>// 拷贝到服务器的目录 /usr/local/sunlight/xxm-ui/xxm-ui</span></span></code></pre></div>`,4),l=[t];function i(c,o,r,u,d,h){return n(),s("div",null,l)}const m=a(e,[["render",i]]);export{g as __pageData,m as default};
