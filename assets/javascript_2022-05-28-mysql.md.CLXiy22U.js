08c0e18b3deffa45336752f94b79f5a1984c2055
aehyok <aehyok@users.noreply.github.com>
2025-05-29 03:25:35 +0000

    deploy: 772d934f9440adebf1247bb04010e6a8c10f7512

---
+++ assets/javascript_2022-05-28-mysql.md.CLXiy22U.js
@@ -0,0 +1,21 @@
+import{_ as e,c as t,o as n,ag as o}from"./chunks/framework.Crtr1jMB.js";const p=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"javascript/2022-05-28-mysql.md","filePath":"javascript/2022-05-28-mysql.md","lastUpdated":1748489095000}'),r={name:"javascript/2022-05-28-mysql.md"};function l(s,a,i,c,h,d){return n(),t("div",null,a[0]||(a[0]=[o(`<h3 id="linux-centos-mysql-安装和使用" tabindex="-1">linux centos mysql 安装和使用 <a class="header-anchor" href="#linux-centos-mysql-安装和使用" aria-label="Permalink to &quot;linux centos mysql 安装和使用&quot;">​</a></h3><ul><li><a href="https://cloud.tencent.com/developer/article/1393323" target="_blank" rel="noreferrer">https://cloud.tencent.com/developer/article/1393323</a></li><li><a href="https://www.cnblogs.com/yuanchao-blog/p/10679156.html" target="_blank" rel="noreferrer">https://www.cnblogs.com/yuanchao-blog/p/10679156.html</a></li><li><a href="https://cloud.tencent.com/developer/article/1145484?from=14588" target="_blank" rel="noreferrer">https://cloud.tencent.com/developer/article/1145484?from=14588</a></li></ul><h3 id="window-java-idea搭建" tabindex="-1">window java idea搭建 <a class="header-anchor" href="#window-java-idea搭建" aria-label="Permalink to &quot;window java idea搭建&quot;">​</a></h3><pre><code>- 30天后评估   https://www.exception.site/essay/idea-reset-eval
+- java demo项目  https://www.exception.site/article/28
+- 配置java环境变量 https://www.cnblogs.com/nojacky/p/9497724.html
+</code></pre><h3 id="maven-包管理器" tabindex="-1">maven 包管理器 <a class="header-anchor" href="#maven-包管理器" aria-label="Permalink to &quot;maven 包管理器&quot;">​</a></h3><pre><code>- 下载地址 http://maven.apache.org/download.cgi
+- https://www.runoob.com/maven/maven-setup.html 安装配置
+- 配置好maven可能需要重启电脑才能再idea中使用mvn -v
+</code></pre><h3 id="nginx-部署springboot-java项目" tabindex="-1">nginx 部署springboot java项目 <a class="header-anchor" href="#nginx-部署springboot-java项目" aria-label="Permalink to &quot;nginx 部署springboot java项目&quot;">​</a></h3><pre><code>- 部署 https://blog.csdn.net/weixin_31992389/article/details/114082476    
+- linux下下载java和配置环境变量 https://blog.csdn.net/sizaif/article/details/106106955
+- 开启 nohup java -jar demo-0.0.1.jar &gt; log.log 2&gt;&amp;1 &amp;
+- chmod +x /usr/share/nginx/html/java/startup.sh
+- chmod +x /usr/local/project/startup.sh
+- su - root -c &#39;/usr/share/nginx/html/java/java.sh&#39;
+- 设置 java开启运行启动命令 https://www.jianshu.com/p/3f62252c9042
+- https://www.cnblogs.com/miles322/p/13755212.html
+
+- nohup 可执行程序的配置使用 https://blog.csdn.net/qq_40241957/article/details/98584207
+- log4j记录日志使用 https://blog.csdn.net/weixin_35696071/article/details/114343607
+</code></pre><h3 id="mysql-常用基本操作" tabindex="-1">mysql 常用基本操作 <a class="header-anchor" href="#mysql-常用基本操作" aria-label="Permalink to &quot;mysql 常用基本操作&quot;">​</a></h3><pre><code>- mysql中 information_schema数据库的作用  https://blog.csdn.net/xianjie0318/article/details/80857568
+- TABLES 关于数据库中表的信息 select * from \`TABLES\` where TABLE_SCHEMA=&#39;aehyok&#39;
+- COLUMNS 关于数据库中的表的字段信息   select * from \`COLUMNS\` where TABLE_NAME=&#39;user&#39;
+</code></pre><h2 id="_1、" tabindex="-1">1、 <a class="header-anchor" href="#_1、" aria-label="Permalink to &quot;1、&quot;">​</a></h2>`,11)]))}const u=e(r,[["render",l]]);export{p as __pageData,u as default};
