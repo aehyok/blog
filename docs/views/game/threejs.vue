675713cba5a629c71b2436afae50d43ed50e78f9
刘启明 <455043818@qq.com>
2025-07-08 18:40:31 +0800

    chore:跳一跳小例子

--- docs/views/game/threejs.vue
+++ docs/views/game/threejs.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="game" ref="gameContainer" class="game-container">
     <div class="info">
-      <h2>Three.js Vue3 入门例子</h2>
-      <p>一个旋转的绿色立方体</p>
+      <h2>跳一跳模拟小游戏</h2>
       <div id="score">当前分数: {{state.score}}</div>
     </div>
   </div>
@@ -12,6 +11,8 @@
 import * as THREE from 'three';
 import { onMounted, onUnmounted, ref, reactive } from 'vue';
 
+let animationId: number;
+
 // 响应式引用
 const gameContainer = ref<HTMLElement | null>(null);
 
@@ -19,24 +20,11 @@ const gameContainer = ref<HTMLElement | null>(null);
 let scene: THREE.Scene;
 let camera: THREE.PerspectiveCamera;
 let renderer: THREE.WebGLRenderer;
-let cube: THREE.Mesh;
-let animationId: number;
 // 移动的小人
 let player;
 
-// camera 聚焦位置
-let focusPos = { x: 0, y: 0, z: 0 };
-
-const targetCameraPos = { x: 100, y: 100, z: 100 };
-
 // 每次移动的起始位置
 const cameraFocus = { x: 0, y: 0, z: 0 };
-// 每次移动后的终点位置
-const targetCameraFocus = { x: 0, y: 0, z: 0 };
-
-
-const playerPos = { x: 0, y: 17.5, z: 0};
-const targetPlayerPos = { x: 0, y: 17.5, z: 0};
 
 const currentCubePos = {x: 0, y: 0, z: 0};
 let direction = 'right';
@@ -47,17 +35,10 @@ let speedY = 0;
 
 let stopped = false;
 const state = reactive({
-  score: -1
+  score: 0
 })
 
-function speedUp() {
-  if(pressed) {
-      speed += 0.1;
-      speedY += 0.1;
-  }
-}
-
-// 获取窗口尺寸
+// 设置窗口宽高移动端待适配
 const getWindowSize = () => ({
   width: 1200,
   height: 600
@@ -70,7 +51,7 @@ function initThreeJS() {
   // 创建场景
   scene = new THREE.Scene();
   
-  // 创建相机 - 调整视野角度和位置
+  // 创建相机 - 调整视野角度和位置 （45度，宽高比，看到最近处0.1 最远处1000）
   camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
   camera.position.set(100, 100, 100);
   camera.lookAt(scene.position); // 确保相机朝向场景中心
@@ -78,21 +59,20 @@ function initThreeJS() {
   // 创建渲染器
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(width, height);
-  renderer.setClearColor(0x333333, 1); // 设置渲染器背景色
-  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setClearColor(0xc6c9d2); // 设置渲染器背景色
 
   // 添加光源
   addLights();
 
-  // 创建立方体(预留两个)
-  createCube(0, 0);
-  // createCube(0, -100);
+  // 添加小人玩家（还待优化）
+  player = createPlayer();
 
+  // 创建立方体
+  createCube(0, 0);
 
   //添加坐标辅助器
   addAxesHelper();
   
-  player = createPlayer();
   // 添加到DOM
   if (gameContainer.value) {
     gameContainer.value.appendChild(renderer.domElement);
@@ -105,150 +85,64 @@ function initThreeJS() {
     document.body.addEventListener('mouseup', () => {
       pressed = false; // 确定按钮按压结束
     })
-
-    document.body.addEventListener('click', () => {
-      // // 向右则是z轴减100
-      // if(direction === 'right') {
-      //   targetCameraPos.z = camera.position.z - 100;
-      //   targetCameraFocus.z -= 100;
-      //   targetPlayerPos.z -= 100;
-      // }
-      // else { // 向左x轴减100
-      //   targetCameraPos.x = camera.position.x - 100
-      //   targetCameraFocus.x -= 100
-      //   targetPlayerPos.x -=100;
-      // }
-
-      // speed = 5;
-
-      // // 操作完毕，预设出下一次跳动的方向
-      // const num = Math.random();
-      // if(num > 0.5) {
-      //     currentCubePos.z -= 100;
-      //     direction = 'right';
-      // } else {
-      //     currentCubePos.x -= 100;
-      //     direction = 'left';
-      // }
-      // createCube(currentCubePos.x, currentCubePos.z);
-    });
   }
 }
-
-function playerInCube() {
-    if(direction === 'right') {
-        if( player.position.z < currentCubePos.z + 15 && player.position.z > currentCubePos.z - 15) {
-            return true;
-        }
-    } else if(direction === 'left') {
-        if( player.position.x < currentCubePos.x + 15 && player.position.x > currentCubePos.x - 15) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function movePlayer() {
-  player.position.y += speedY;
-  
-  if(player.position.y < 17.5) {
-    player.position.y = 17.5;
-
-    if(!stopped){
-      if(playerInCube()) {
-        state.score ++;
-        console.log("score++分数:", state.score);
-        return;
-      }
-      else {
-        console.log("失败了，当前分数为:" +state.score)
-      }
-
-      const distance = Math.floor(50 + Math.random() * 100);
-
-      const num = Math.random();
-      if(num > 0.5) {
-          currentCubePos.z -= distance;
-          direction = 'right';
-      } else {
-          currentCubePos.x -= distance;
-          direction = 'left';
-      }
-      createCube(currentCubePos.x, currentCubePos.z);
-    }
-    stopped = true;
-
-  } else {
-    stopped = false;
-    if(direction === 'right') {
-        player.position.z -= speed;
-    } else {
-        player.position.x -= speed;
-    }
-  }
-  speedY -= 0.3;
-}
-
-function moveCamera() {
-  if(player.position.y > 17.5) {
-    if(direction === 'right') {
-        camera.position.z -= speed;
-        cameraFocus.z -= speed;
-    } else {
-        camera.position.x -= speed;
-        cameraFocus.x -= speed;
-    }
-    
-    camera.lookAt(cameraFocus.x, cameraFocus.y, cameraFocus.z);
-  }
-}
-
-// function moveCamera() {
-//   const { x, z } = camera.position;
-//   if(x > targetCameraPos.x) {
-//       camera.position.x -= 3;
-//   }
-//   if(z > targetCameraPos.z) {
-//       camera.position.z -= 3;
-//   }
-
-//   if(cameraFocus.x > targetCameraFocus.x) {
-//       cameraFocus.x -= 3;
-//   }
-//   if(cameraFocus.z > targetCameraFocus.z) {
-//       cameraFocus.z -= 3;
-//   }
-
-//   camera.lookAt(cameraFocus.x, cameraFocus.y, cameraFocus.z);  
-// }
-
 // function movePlayer() {
-//   if(player.position.x > targetPlayerPos.x) {
-//       player.position.x -= 3;
-//   }
-//   if(player.position.z > targetPlayerPos.z) {
-//       player.position.z -= 3;
-//   }
-//   player.position.y += speed;
-//   speed -= 0.3;
+//   player.position.y += speedY;
 
 //   if(player.position.y < 17.5) {
-//       player.position.y = 17.5;
+//     player.position.y = 17.5;
+
+//     if(!stopped){
+//       if(playerInCube()) {
+//         state.score ++;
+//         console.log("score++分数:", state.score);
+//         return;
+//       }
+//       else {
+//         console.log("失败了，当前分数为:" +state.score)
+//       }
+
+//       const distance = Math.floor(50 + Math.random() * 100);
+
+//       const num = Math.random();
+//       if(num > 0.5) {
+//           currentCubePos.z -= distance;
+//           direction = 'right';
+//       } else {
+//           currentCubePos.x -= distance;
+//           direction = 'left';
+//       }
+//       createCube(currentCubePos.x, currentCubePos.z);
+//     }
+//     stopped = true;
+
+//   } else {
+//     stopped = false;
+//     if(direction === 'right') {
+//         player.position.z -= speed;
+//     } else {
+//         player.position.x -= speed;
+//     }
 //   }
+//   speedY -= 0.3;
 // }
 
+
+// 创建立方体
 function createCube(x, z) {
   const geometry = new THREE.BoxGeometry( 30, 20, 30 );
-  const material = new THREE.MeshPhongMaterial( {color: 0xffffff} );
+  const material = new THREE.MeshPhongMaterial( {color: 0xf6f6f6} );
   const cube = new THREE.Mesh( geometry, material ); 
   cube.position.x = x;
   cube.position.z = z;
   scene.add( cube );
 }
 
+// 创建小人玩家头像
 function createPlayer() {
   const geometry = new THREE.BoxGeometry( 5, 15, 5 );
-  const material = new THREE.MeshPhongMaterial( {color: 0x000000} );
+  const material = new THREE.MeshPhongMaterial( {color:0x3c364e } );
   const player = new THREE.Mesh( geometry, material); 
   player.position.x = 0;
   player.position.y = 17.5;
@@ -257,6 +151,7 @@ function createPlayer() {
   return player;
 }
 
+// 添加坐标系
 const addAxesHelper = () => {
   const axesHelper = new THREE.AxesHelper( 1000 );
   axesHelper.position.set(0,0,0);
@@ -265,10 +160,100 @@ const addAxesHelper = () => {
 
 // 添加光源
 function addLights() {
-  const directionalLight = new THREE.DirectionalLight( 0xffffff , 1.0 );
+  const directionalLight = new THREE.DirectionalLight( 0xffffff );
   directionalLight.position.set(40, 100, 60);
-
   scene.add( directionalLight );
+}
+
+function moveCamera() {
+    if(player.position.y > 17.5) {
+        if(direction === 'right') {
+            camera.position.z -= speed;
+            cameraFocus.z -= speed;
+        } else {
+            camera.position.x -= speed;
+            cameraFocus.x -= speed;
+        }
+        
+        camera.lookAt(cameraFocus.x, cameraFocus.y, cameraFocus.z);
+    }
+}
+
+function playerInCube() {
+  if(direction === 'right') {
+      if( player.position.z < currentCubePos.z + 15 && player.position.z > currentCubePos.z - 15) {
+          return true;
+      }
+  } else if(direction === 'left') {
+      if( player.position.x < currentCubePos.x + 15 && player.position.x > currentCubePos.x - 15) {
+          return true;
+      }
+  }
+  return false;
+}
+
+function movePlayer() {
+    player.position.y += speedY;
+
+    if(player.position.y < 17.5) {
+        player.position.y = 17.5;
+
+        if(stopped === false){
+
+            if(playerInCube()) {
+                state.score+= 100;
+                // document.getElementById('score').innerHTML = score;
+
+                const distance = Math.floor(50 + Math.random() * 100);
+
+                const num = Math.random();
+                if(num > 0.5) {
+                    currentCubePos.z -= distance;
+                    direction = 'right';
+                } else {
+                    currentCubePos.x -= distance;
+                    direction = 'left';
+                }
+                createCube(currentCubePos.x, currentCubePos.z);
+            } else {
+              alert("失败了，当前分数为:" + state.score);
+              // setTimeout(() => {
+              //   window.location.reload();
+              // }, 500);
+                // document.getElementById('fail').style.display = 'block';
+                // document.getElementById('score2').innerHTML = score;
+            }
+
+        }
+
+        stopped = true;
+    } else {
+        stopped = false;
+        if(direction === 'right') {
+            player.position.z -= speed;
+        } else {
+            player.position.x -= speed;
+        }
+    }
+    speedY -= 0.3;
+}
+
+function speedUp() {
+  if(pressed) {
+      speed += 0.1;
+      speedY += 0.1;
+
+      if(player.scale.y> 0) {
+          player.scale.y -= 0.001;
+      }
+      player.position.y -= 15 - 15 * player.scale.y
+
+      if(player.position.y < 10) {
+          player.position.y = 10;
+      }
+  } else {
+      player.scale.y = 1;
+  }
 }
 
 // 渲染循环
@@ -282,7 +267,7 @@ function render() {
 
   // 渲染场景
   renderer.render(scene, camera);
-  requestAnimationFrame(render);
+  animationId = requestAnimationFrame(render);
 }
 
 // 处理窗口大小变化
@@ -297,11 +282,8 @@ function onWindowResize() {
 
 // 组件挂载时初始化
 onMounted(() => {
-  console.log(THREE, "--THREE加载成功--");
-  
   initThreeJS();
   render();
-  
   // 监听窗口大小变化
   window.addEventListener('resize', onWindowResize);
 });
@@ -309,8 +291,9 @@ onMounted(() => {
 // 组件卸载时清理
 onUnmounted(() => {
   //停止动画循环
-  if (animationId){
-    cancelAnimationFrame();
+  if(animationId){
+    cancelAnimationFrame(animationId);
+    animationId = 0;
   }
   
   // 清理Three.js对象
