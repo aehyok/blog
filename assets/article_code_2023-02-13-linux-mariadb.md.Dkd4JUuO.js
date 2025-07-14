c1e6d217030c0c0b21be7478b414bd53319b56ba
aehyok <aehyok@users.noreply.github.com>
2025-07-14 10:23:57 +0000

    deploy: 55ca236126e3edd43e347a24828b9696b248448d

---
+++ assets/article_code_2023-02-13-linux-mariadb.md.Dkd4JUuO.js
@@ -0,0 +1,20 @@
+import{_ as s,c as n,o as e,ag as p}from"./chunks/framework.mJdS8VI0.js";const u=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"article/code/2023-02-13-linux-mariadb.md","filePath":"article/code/2023-02-13-linux-mariadb.md","lastUpdated":1752488591000}'),i={name:"article/code/2023-02-13-linux-mariadb.md"};function l(t,a,c,r,d,o){return e(),n("div",null,a[0]||(a[0]=[p(`<h2 id="linux" tabindex="-1">linux <a class="header-anchor" href="#linux" aria-label="Permalink to &quot;linux&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>ip 用户名 密码</span></span>
+<span class="line"><span>175.178.60.76 root  Hk</span></span></code></pre></div><h2 id="设置mariadb源" tabindex="-1">设置mariadb源 <a class="header-anchor" href="#设置mariadb源" aria-label="Permalink to &quot;设置mariadb源&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>cd /etc/yum.repos.d</span></span>
+<span class="line"><span>vim Mariadb.repo</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span></span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>[mariadb]</span></span>
+<span class="line"><span>name = MariaDB</span></span>
+<span class="line"><span>baseurl = https://mirrors.aliyun.com/mariadb/yum/10.4/centos7-amd64</span></span>
+<span class="line"><span>gpgkey = https://mirrors.aliyun.com/mariadb/yum/RPM-GPG-KEY-MariaDB</span></span>
+<span class="line"><span>gpgcheck = 1</span></span></code></pre></div><h2 id="mariadb" tabindex="-1">mariadb <a class="header-anchor" href="#mariadb" aria-label="Permalink to &quot;mariadb&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>yum install MariaDB-server</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>systemctl enable mariadb</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>systemctl start mariadb</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>// 设置密码 M9</span></span>
+<span class="line"><span>mysql_secure_installation</span></span></code></pre></div><h2 id="可能会乱码设置" tabindex="-1">可能会乱码设置 <a class="header-anchor" href="#可能会乱码设置" aria-label="Permalink to &quot;可能会乱码设置&quot;">​</a></h2><ul><li><a href="https://juejin.cn/post/7096757877909487629" target="_blank" rel="noreferrer">https://juejin.cn/post/7096757877909487629</a></li></ul><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>utf8mb4</span></span>
+<span class="line"><span>utf8mb4_unicode_ci</span></span></code></pre></div>`,9)]))}const m=s(i,[["render",l]]);export{u as __pageData,m as default};
---
+++ assets/article_code_2024-07-24-query.md.CNeKtbo7.js
@@ -0,0 +1,17 @@
+import{_ as s,c as n,o as e,ag as t}from"./chunks/framework.mJdS8VI0.js";const q=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"article/code/2024-07-24-query.md","filePath":"article/code/2024-07-24-query.md","lastUpdated":1752488591000}'),p={name:"article/code/2024-07-24-query.md"};function o(l,a,i,u,r,c){return e(),n("div",null,a[0]||(a[0]=[t(`<h2 id="菜单" tabindex="-1">菜单 <a class="header-anchor" href="#菜单" aria-label="Permalink to &quot;菜单&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>## 先通过root 获取根菜单</span></span>
+<span class="line"><span>select * from md_sysparameters s where s.name like &#39;%root%&#39; and systemid=&#39;dev&#39;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>## 然后再通过MenuID获取下级菜单</span></span>
+<span class="line"><span>select * from md_sysparameters s where s.\`name\` like &#39;%90000&#39; and systemid=&#39;dev&#39;</span></span></code></pre></div><h2 id="数据导入菜单" tabindex="-1">数据导入菜单 <a class="header-anchor" href="#数据导入菜单" aria-label="Permalink to &quot;数据导入菜单&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>{</span></span>
+<span class="line"><span>    &quot;MenuID&quot;: &quot;60000&quot;,</span></span>
+<span class="line"><span>    &quot;MenuTitle&quot;: &quot;乡村摸底调查数据导入&quot;,</span></span>
+<span class="line"><span>    &quot;DisplayOrder&quot;: 25,</span></span>
+<span class="line"><span>    &quot;FatherID&quot;: &quot;0&quot;,</span></span>
+<span class="line"><span>    &quot;IconName&quot;: &quot;&quot;,</span></span>
+<span class="line"><span>    &quot;MenuType&quot;: &quot;LoadFromServer&quot;,</span></span>
+<span class="line"><span>    &quot;MenuParameter&quot;: &quot;&quot;,</span></span>
+<span class="line"><span>    &quot;CanUse&quot;: true,</span></span>
+<span class="line"><span>    &quot;Level&quot;: 1,</span></span>
+<span class="line"><span>    &quot;IconIndex&quot;: &quot;-1&quot;,</span></span>
+<span class="line"><span>    &quot;Children&quot;: []</span></span>
+<span class="line"><span>}</span></span></code></pre></div>`,4)]))}const h=s(p,[["render",o]]);export{q as __pageData,h as default};
