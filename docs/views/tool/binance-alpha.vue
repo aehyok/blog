2c55e89fa98e0d76f60f8ac089dfa9f7bf28b7bb
刘启明 <455043818@qq.com>
2025-07-22 18:29:41 +0800

    feat: 测试接口

--- docs/views/tool/binance-alpha.vue
+++ docs/views/tool/binance-alpha.vue
@@ -1,4 +1,21 @@
 <template>
   <div>111-222</div>
 </template>
-<script setup></script>
+<script setup>
+  import { onMounted } from 'vue'
+  import axios from 'axios'
+  onMounted(async() => {
+    // 这里可以添加一些初始化代码
+    console.log('Binance Alpha component mounted')
+    try {
+    const url = 'https://api.etherscan.io/v2/api?chainid=56&module=account&action=tokentx&address=0xf05E440e444C1629F73caF142f7aad09ED9453ca&startblock=0&endblock=99999999&page=1&offset=10&sort=desc&apikey=JGKUPIJS5BK9PFKHPQB7Y2SRFJYD7QX52K';
+    
+    const response = await axios.get(url);
+    console.log('交易数据:', response.data);
+      response.data;
+  } catch (error) {
+    console.error('请求失败:', error.response?.data || error.message);
+    throw error;
+  }
+  })
+</script>
