a3a860b28ab8121a14c5ec9c8582c88fdd23c831
刘启明 <455043818@qq.com>
2025-09-19 07:24:44 +0800

    Update index.vue

--- docs/alipay/index.vue
+++ docs/alipay/index.vue
@@ -92,6 +92,13 @@ import { ref, reactive, computed, onMounted } from 'vue'
 // 响应式数据
 const records = ref([
 {
+  date: '2025.9.18',
+  week: "星期四",
+  period: '6',
+  status: '1',
+  index: 20,
+},
+{
   date: '2025.9.17',
   week: "星期三",
   period: '5',
