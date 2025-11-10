7899ab2b4438484f25c4dd12777e20d446ea95c8
aehyok <aehyok@users.noreply.github.com>
2025-11-10 10:31:20 +0000

    deploy: 2a14177cdd517d7310e659853d54e04b54b46336

---
+++ assets/article_code_2023-11-01-xe.md.D7yLk71Y.js
@@ -0,0 +1,62 @@
+import{_ as t,c as p,o as e,ag as n}from"./chunks/framework.ELrfyV9R.js";const o=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"article/code/2023-11-01-xe.md","filePath":"article/code/2023-11-01-xe.md","lastUpdated":1762770629000}'),a={name:"article/code/2023-11-01-xe.md"};function c(d,s,l,r,h,_){return e(),p("div",null,s[0]||(s[0]=[n(`<p>-- 状态记录表 select * from CollectDataStatus c where c.hid=&#39;77c4321e-0f92-45eb-a934-4d2d464431c5&#39;</p><p>-- 基本信息 select * from pcs2309_tph_jbxx c where c.hid=&#39;77c4321e-0f92-45eb-a934-4d2d464431c5&#39;</p><p>-- 收入情况 select * from pcs2309_tph_srqk c where c.hid=&#39;77c4321e-0f92-45eb-a934-4d2d464431c5&#39;</p><p>-- 家庭成员 select * from pcs2309_tph_jtcy c where c.hid=&#39;77c4321e-0f92-45eb-a934-4d2d464431c5&#39;</p><p>-- 帮扶责任人 select * from pcs2309_tph_bfzrr c where c.hid=&#39;77c4321e-0f92-45eb-a934-4d2d464431c5&#39;</p><p>--生产生活条件 select * from pcs2309_tph_scshtj c where c.hid=&#39;77c4321e-0f92-45eb-a934-4d2d464431c5&#39;</p><p>--- 户主信息 select * from pcs2309_householdcode c where c.id=&#39;77c4321e-0f92-45eb-a934-4d2d464431c5&#39;</p><p>update <code>pcs2309_tph_srqk</code> set e9 =0 where e9 is null;</p><p>update <code>pcs2309_tph_srqk</code> set e8 =0 where e8 is null;</p><p>update <code>pcs2309_tph_srqk</code> set e4 =0 where e4 is null;</p><p>update <code>pcs2309_tph_srqk</code> set e30 =0 where e30 is null;</p><p>update <code>pcs2309_tph_srqk</code> set e2 =0 where e2 is null;</p><p>update <code>pcs2309_tph_srqk</code> set e1 =0 where e1 is null;</p><p>update <code>pcs2309_tph_srqk</code> set e =0 where e is null;</p><p>update <code>pcs2309_tph_srqk</code> set d9 =0 where d9 is null;</p><p>update <code>pcs2309_tph_srqk</code> set d8 =0 where d8 is null;</p><p>update <code>pcs2309_tph_srqk</code> set d6p=0 where d6p is null;</p><p>update <code>pcs2309_tph_srqk</code> set d6o=0 where d6o is null;</p><p>update <code>pcs2309_tph_srqk</code> set d6n=0 where d6n is null;</p><p>update <code>pcs2309_tph_srqk</code> set d6m=0 where d6m is null;</p><p>update <code>pcs2309_tph_srqk</code> set d6l=0 where d6l is null;</p><p>update <code>pcs2309_tph_srqk</code> set d6k=0 where d6k is null;</p><p>update <code>pcs2309_tph_srqk</code> set d6j=0 where d6j is null;</p><p>update <code>pcs2309_tph_srqk</code> set d6i=0 where d6i is null;</p><p>update <code>pcs2309_tph_srqk</code> set d6h=0 where d6h is null;</p><p>update <code>pcs2309_tph_srqk</code> set d6g=0 where d6g is null;</p><p>update <code>pcs2309_tph_srqk</code> set d6f=0 where d6f is null;</p><p>update <code>pcs2309_tph_srqk</code> set d6e=0 where d6e is null;</p><p>update <code>pcs2309_tph_srqk</code> set d6d=0 where d6d is null;</p><p>update <code>pcs2309_tph_srqk</code> set d6c=0 where d6c is null;</p><p>update <code>pcs2309_tph_srqk</code> set d6b=0 where d6b is null;</p><h2 id="update-pcs2309-tph-srqk-set-d6a-0-where-d6a-is-null" tabindex="-1">update <code>pcs2309_tph_srqk</code> set d6a=0 where d6a is null; <a class="header-anchor" href="#update-pcs2309-tph-srqk-set-d6a-0-where-d6a-is-null" aria-label="Permalink to &quot;update  \`pcs2309_tph_srqk\` set  d6a=0 where d6a is null;&quot;">​</a></h2><p>update <code>pcs2309_tph_srqk</code> set d6=0 where d6 is null;</p><p>update <code>pcs2309_tph_srqk</code> set d5e=0 where d5e is null;</p><p>update <code>pcs2309_tph_srqk</code> set d5d=0 where d5d is null;</p><p>update <code>pcs2309_tph_srqk</code> set d5c=0 where d5c is null;</p><p>update <code>pcs2309_tph_srqk</code> set d5b=0 where d5b is null;</p><p>update <code>pcs2309_tph_srqk</code> set d5a=0 where d5a is null;</p><p>update <code>pcs2309_tph_srqk</code> set d5=0 where d5 is null;</p><p>update <code>pcs2309_tph_srqk</code> set d4=0 where d4 is null;</p><p>update <code>pcs2309_tph_srqk</code> set d3=0 where d3 is null;</p><p>update <code>pcs2309_tph_srqk</code> set d2=0 where d2 is null;</p><p>update <code>pcs2309_tph_srqk</code> set d1=0 where d1 is null;</p><p>update <code>pcs2309_tph_srqk</code> set d=0 where d is null;</p><p>update <code>pcs2309_tph_srqk</code> set lpsr=0 where lpsr is null;</p><p>update <code>pcs2309_tph_srqk</code> set hgzfzc=0 where hgzfzc is null;</p><p>update <code>pcs2309_tph_srqk</code> set a=0 where a is null;</p><p>update <code>pcs2309_tph_srqk</code> set a1=0 where a1 is null;</p><p>update <code>pcs2309_tph_srqk</code> set a2=0 where a2 is null;</p><p>update <code>pcs2309_tph_srqk</code> set a3=0 where a3 is null;</p><p>update <code>pcs2309_tph_srqk</code> set a4=0 where a4 is null;</p><p>update <code>pcs2309_tph_srqk</code> set a5=0 where a5 is null;</p><p>update <code>pcs2309_tph_srqk</code> set a8=0 where a8 is null;</p><p>update <code>pcs2309_tph_srqk</code> set a9=0 where a9 is null;</p><p>update <code>pcs2309_tph_srqk</code> set a10=0 where a10 is null;</p><p>update <code>pcs2309_tph_srqk</code> set b=0 where b is null;</p><p>update <code>pcs2309_tph_srqk</code> set b1=0 where b1 is null;</p><p>update <code>pcs2309_tph_srqk</code> set b2=0 where b2 is null;</p><p>update <code>pcs2309_tph_srqk</code> set b3=0 where b3 is null;</p><p>update <code>pcs2309_tph_srqk</code> set b4=0 where b4 is null;</p><p>update <code>pcs2309_tph_srqk</code> set c=0 where c is null;</p><p>update <code>pcs2309_tph_srqk</code> set c1=0 where c1 is null;</p><p>update <code>pcs2309_tph_srqk</code> set c2=0 where c2 is null;</p><p>update <code>pcs2309_tph_srqk</code> set c3=0 where c3 is null;</p><p>update <code>pcs2309_tph_srqk</code> set c4=0 where c4 is null;</p><p>update <code>pcs2309_tph_srqk</code> set c5=0 where c5 is null;</p><p>select * from pcs2309_tph_srqk</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>CREATE DEFINER=\`root\`@\`%\` PROCEDURE \`ffpfun_calculateincome_2309\`(in p_hid varchar(200) CHARSET utf8)</span></span>
+<span class="line"><span>BEGIN</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>  #定义一个变量用于计录户总人数</span></span>
+<span class="line"><span>  DECLARE d_zrs DECIMAL(10,2) DEFAULT 0;	</span></span>
+<span class="line"><span>	#定义一个变量用于计录户类型</span></span>
+<span class="line"><span>	DECLARE d_hlx VARCHAR(255);	</span></span>
+<span class="line"><span>  #定义一个错误的变量,类型是整形，默认是0</span></span>
+<span class="line"><span>  DECLARE t_error INTEGER DEFAULT 0;</span></span>
+<span class="line"><span>  #捕获到sql的错误，就设置t_error为1</span></span>
+<span class="line"><span>  DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET t_error=1;</span></span>
+<span class="line"><span>		</span></span>
+<span class="line"><span>  #开启事务</span></span>
+<span class="line"><span>  START TRANSACTION;	</span></span>
+<span class="line"><span>	</span></span>
+<span class="line"><span>	# 取此户的总人数...	</span></span>
+<span class="line"><span>	select IFNULL(ndrks,1),hlx into d_zrs,d_hlx from pcs2309_householdcode where id=p_hid ;</span></span>
+<span class="line"><span>	</span></span>
+<span class="line"><span>	# 为保证算人均时不出错，如果户总人口数小于1，则改为1</span></span>
+<span class="line"><span>	IF d_zrs&lt;1 THEN</span></span>
+<span class="line"><span>	   SET d_zrs=1;</span></span>
+<span class="line"><span>  END IF;</span></span>
+<span class="line"><span>	</span></span>
+<span class="line"><span>	# 计算收入情况</span></span>
+<span class="line"><span>	# 1.计算D5,D6</span></span>
+<span class="line"><span>	update pcs2309_tph_srqk t set t.D5 = IFNULL(t.D5a,0)+IFNULL(t.D5b,0)+IFNULL(t.D5c,0)+IFNULL(t.D5d,0)+IFNULL(t.D5e,0),</span></span>
+<span class="line"><span>															  t.D6 = IFNULL(t.D6a,0)+IFNULL(t.D6b,0)+IFNULL(t.D6c,0)+IFNULL(t.D6d,0)+IFNULL(t.D6e,0)</span></span>
+<span class="line"><span>																      +IFNULL(t.D6f,0)+IFNULL(t.D6g,0)+IFNULL(t.D6h,0)+IFNULL(t.D6i,0)+IFNULL(t.D6j,0)</span></span>
+<span class="line"><span>																			+IFNULL(t.D6k,0)+IFNULL(t.D6l,0)+IFNULL(t.D6m,0)+IFNULL(t.D6n,0)+IFNULL(t.D6o,0)+IFNULL(t.D6p,0)</span></span>
+<span class="line"><span>														where t.hid=p_hid and t.nf=&#39;202309&#39;;</span></span>
+<span class="line"><span>																</span></span>
+<span class="line"><span>	# 2.计算各项收入和支出大项</span></span>
+<span class="line"><span>	update pcs2309_tph_srqk t set t.A=IFNULL(t.A1,0)+IFNULL(t.A2,0)+IFNULL(t.A3,0)+IFNULL(t.A4,0)+IFNULL(t.A5,0), </span></span>
+<span class="line"><span>	                              t.B=IFNULL(t.B1,0)+IFNULL(t.B2,0)+IFNULL(t.B3,0)+IFNULL(t.B4,0), </span></span>
+<span class="line"><span>																t.C=IFNULL(t.C1,0)+IFNULL(t.C2,0)+IFNULL(t.C3,0)+IFNULL(t.C4,0)+IFNULL(t.C5,0),</span></span>
+<span class="line"><span>																t.D=IFNULL(t.D1,0)+IFNULL(t.D2,0)+IFNULL(t.D3,0)+IFNULL(t.D4,0)+IFNULL(t.D5,0)+IFNULL(t.D6,0),</span></span>
+<span class="line"><span>																t.E=IFNULL(t.E1,0)+IFNULL(t.E2,0)+IFNULL(t.E3,0)+IFNULL(t.E4,0)</span></span>
+<span class="line"><span>														where t.hid=p_hid and t.nf=&#39;202309&#39;;</span></span>
+<span class="line"><span>	</span></span>
+<span class="line"><span>	# 3.计算总计</span></span>
+<span class="line"><span>	update pcs2309_tph_srqk t set t.nzsr = IFNULL(t.A,0)+IFNULL(t.B,0)+IFNULL(t.C,0)+IFNULL(t.D,0),</span></span>
+<span class="line"><span>															  t.ncsr = IFNULL(t.A,0)+IFNULL(t.B,0)+IFNULL(t.C,0)+IFNULL(t.D,0) - IFNULL(t.E,0),</span></span>
+<span class="line"><span>																t.rjcsr = (IFNULL(t.A,0)+IFNULL(t.B,0)+IFNULL(t.C,0)+IFNULL(t.D,0) - IFNULL(t.E,0)) /d_zrs																</span></span>
+<span class="line"><span>																where t.hid=p_hid and t.nf=&#39;202309&#39;;</span></span>
+<span class="line"><span>										</span></span>
+<span class="line"><span>	# 4.如果是监测户，计算纳入监测收入范围</span></span>
+<span class="line"><span>	IF d_hlx=&#39;100B02002&#39; THEN</span></span>
+<span class="line"><span>		 update pcs2309_tph_srqk t set t.zsrckfw = IFNULL(t.ncsr,0) + IFNULL(t.lpsr,0) - IFNULL(t.hgzfzc,0),</span></span>
+<span class="line"><span>																	 t.rjsrckfw = (IFNULL(t.ncsr,0) + IFNULL(t.lpsr,0) - IFNULL(t.hgzfzc,0)) /d_zrs</span></span>
+<span class="line"><span>																where t.hid=p_hid and t.nf=&#39;202309&#39;;</span></span>
+<span class="line"><span>	END IF;</span></span>
+<span class="line"><span>	</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>  #如果捕获到错误</span></span>
+<span class="line"><span>    IF t_error=1 THEN</span></span>
+<span class="line"><span>        #回滚</span></span>
+<span class="line"><span>        ROLLBACK;</span></span>
+<span class="line"><span>    ELSE</span></span>
+<span class="line"><span>        #提交</span></span>
+<span class="line"><span>        COMMIT;</span></span>
+<span class="line"><span>    END IF;</span></span>
+<span class="line"><span>END</span></span></code></pre></div>`,68)]))}const u=t(a,[["render",c]]);export{o as __pageData,u as default};
