b009e72d861f8080497e324a5444eb87b72aae39
aehyok <aehyok@users.noreply.github.com>
2026-04-03 16:02:41 +0000

    deploy: 14c0ba34b58847a4f41a6895944537e638fb4ec6

--- assets/ai_2026-03_2026-03-21-codex-manager.md.BnZut4Ps.js
+++
@@ -1,8 +0,0 @@
-import{_ as s,c as e,o as n,ag as p}from"./chunks/framework.ELrfyV9R.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"ai/2026-03/2026-03-21-codex-manager.md","filePath":"ai/2026-03/2026-03-21-codex-manager.md","lastUpdated":1774480913000}'),t={name:"ai/2026-03/2026-03-21-codex-manager.md"};function i(r,a,c,l,o,d){return n(),e("div",null,a[0]||(a[0]=[p(`<p><a href="https://github.com/cnlimiter/codex-manager" target="_blank" rel="noreferrer">https://github.com/cnlimiter/codex-manager</a></p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>docker run -d \\</span></span>
-<span class="line"><span>  -p 1455:1455 \\</span></span>
-<span class="line"><span>  -e WEBUI_HOST=0.0.0.0 \\</span></span>
-<span class="line"><span>  -e WEBUI_PORT=1455 \\</span></span>
-<span class="line"><span>  -e WEBUI_ACCESS_PASSWORD=aehyok \\</span></span>
-<span class="line"><span>  -v $(pwd)/data:/app/data \\</span></span>
-<span class="line"><span>  --name codex-register \\</span></span>
-<span class="line"><span>  ghcr.io/yunxilyf/codex-register:latest</span></span></code></pre></div>`,2)]))}const g=s(t,[["render",i]]);export{m as __pageData,g as default};
